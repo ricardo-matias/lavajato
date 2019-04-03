b9539a7fe7c0325aac7f1d69e0292ff6bd37ea63
Marcos <mbff1998@gmail.com>
2019-04-03 10:56:45 -0300

    DESESPERO

--- LavaJato/src/java/br/edu/ifpe/recife/model/controller/VeiculoController.java
+++ LavaJato/src/java/br/edu/ifpe/recife/model/controller/VeiculoController.java
@@ -4,11 +4,76 @@
  * and open the template in the editor.
  */
 package br.edu.ifpe.recife.model.controller;
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+import br.edu.ifpe.recife.model.dao.ManagerDao;
+import br.edu.ifpe.recife.model.entities.Veiculo;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+
 
 /**
  *
  * @author ALUNO
  */
-public class VeiculoController {
+@ManagedBean(name = "veiculoController")
+@SessionScoped
+public class VeiculoController implements Serializable {
+   private Veiculo veiculo;
+   private Veiculo selVeiculo;
+   
+   public VeiculoController(){
+       this.veiculo = new Veiculo();
+   }
+   public void inserir(){
+        
+        
+       
+        ManagerDao.getCurrentInstance().insert(this.veiculo);
+        this.veiculo = new Veiculo();
+        
+        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Veiculo Cadastrado com sucesso!"));
+        
+        
+    }
     
+    public void alterar(){
+        ManagerDao.getCurrentInstance().update(this.selVeiculo);
+        
+        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("O veículo foi alterado com sucesso!"));
+        
+        
+    }
+    
+    public List<Veiculo> lerTudo(){
+        return ManagerDao.getCurrentInstance().read("select l from Veiculo l", Veiculo.class);
+    }
+    
+    public void deletar(){
+        ManagerDao.getCurrentInstance().delete(this.selVeiculo);
+        
+        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Veiculo deletado com sucesso!"));
+        
+    }
+
+   
+    public Veiculo getVeiculo() {
+        return veiculo;
+    }
+
+    public void setVeiculo(Veiculo veiculo) {
+        this.veiculo = veiculo;
+    }
+
+    public Veiculo getSelVeiculo() {
+        return selVeiculo;
+    }
+
+    public void setSelVeiculo(Veiculo selVeiculo) {
+        this.selVeiculo = selVeiculo;
+    }
+   
 }
--- LavaJato/src/java/br/edu/ifpe/recife/model/entities/Veiculo.java
+++ LavaJato/src/java/br/edu/ifpe/recife/model/entities/Veiculo.java
@@ -8,26 +8,87 @@ package br.edu.ifpe.recife.model.entities;
 import java.util.Date;
 import java.io.Serializable;
 import javax.persistence.Column;
-import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
+import javax.persistence.Table;
+
 /**
  *
  * @author ALUNO
  */
 
 @Entity
+@Table(name = "tb_veiculo")
 public class Veiculo implements Serializable {
  @Id
  @GeneratedValue(strategy = GenerationType.IDENTITY)
- private int id;
+ private Integer id;
  
  @Column(nullable=false,length=20)
  private String tipo;
+ 
+ @Column(nullable=false,length=20)
+ private String marca;
+ 
+ @Column(nullable=false,length=50)
+ private String modelo;
+ 
+ @Column(nullable=false,length=20)
+ private String placa;
+ 
+ @Column(nullable=false,length=20)
+ private String corDominante;
+
+ 
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public String getCorDominante() {
+        return corDominante;
+    }
+
+    public void setCorDominante(String corDominante) {
+        this.corDominante = corDominante;
+    }
  
  
 }
