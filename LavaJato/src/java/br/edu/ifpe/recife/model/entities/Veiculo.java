e7946b0bebe8155b42bd7b5708447bd9a19adbe1
Marcos <mbff1998@gmail.com>
2019-04-03 09:22:41 -0300

    Second Commit

---
+++ LavaJato/src/java/br/edu/ifpe/recife/model/entities/Veiculo.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifpe.recife.model.entities;
+import br.edu.ifpe.recife.model.entities.Endereco;
+import java.util.Date;
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+/**
+ *
+ * @author ALUNO
+ */
+
+
+public class Veiculo implements Serializable {
+ @Id
+ @GeneratedValue
+}
